69aa1420501b199b5f1b2c42874e174e0718b7eb
Lean Mendoza <leandro@decentraland.org>
2024-09-18 10:57:53 -0300

    final version

--- in-world-ui/src/index.ts
+++ in-world-ui/src/index.ts
@@ -10,64 +10,69 @@ export function main(): void {
 }
 
 async function setupScroll() {
-    const use_texture = true;
+  // true for test in-world-ui and false to get the regular ui
+  const useTexture = true
 
-    const ui = new UiExample()
+  // same as we have in the scrollable-ui
+  const ui = new UiExample()
 
-    const uiCanvas = engine.addEntity();
-    if (use_texture) {
-      UiCanvas.create(uiCanvas, { width: 1024, height: 1024, color: Color4.Black() });
-      ReactEcsRenderer.setTextureRenderer(uiCanvas, ui.render.bind(ui));
-    } else {
-      ReactEcsRenderer.setUiRenderer(ui.render.bind(ui));
-    }
+  // canvas for the ui
+  const uiCanvas = engine.addEntity();
+  if (useTexture) {
+    UiCanvas.create(uiCanvas, { width: 1024, height: 1024, color: Color4.Black() });
+    ReactEcsRenderer.setTextureRenderer(uiCanvas, ui.render.bind(ui));
+  } else {
+    ReactEcsRenderer.setUiRenderer(ui.render.bind(ui));
+  }
 
-    const textShapeEntity = engine.addEntity();
-    TextShape.create(textShapeEntity, {
-        text: "Scrollable UI - Example",
-        fontSize: 20,
-        textColor: Color4.Red(),
-        outlineColor: Color4.Black(),
-        outlineWidth: 0.2
-    });
-    Transform.create(textShapeEntity, {
-      position: Vector3.create(8, 1, 8)
-    });
-    Billboard.create(textShapeEntity);
+  // label to recognisze the scene
+  const textShapeEntity = engine.addEntity();
+  TextShape.create(textShapeEntity, {
+      text: "In World UI - Example",
+      fontSize: 20,
+      textColor: Color4.Red(),
+      outlineColor: Color4.Black(),
+      outlineWidth: 0.2
+  });
+  Transform.create(textShapeEntity, {
+    position: Vector3.create(8, 1, 8)
+  });
+  Billboard.create(textShapeEntity);
 
-    console.log("make display");
-    const uiDisplay = engine.addEntity();
-    Transform.create(uiDisplay, { position: Vector3.create(8,1,8), scale: Vector3.create(2,2,2) });
-    GltfContainer.create(uiDisplay, { src: "models/screen.gltf", visibleMeshesCollisionMask: ColliderLayer.CL_POINTER } );
-    await waitUntilGltfIsLoaded([uiDisplay])
-    
-    console.log("paths:")
-    GltfContainerLoadingState.get(uiDisplay).nodePaths.forEach((p) => {
-      console.log(p)
-    });
-    console.log("end paths")
+  // GLTF part to get the screen mesh to render the UI
+  console.log("make display");
+  const uiDisplay = engine.addEntity();
+  Transform.create(uiDisplay, { position: Vector3.create(8,1,8), scale: Vector3.create(2,2,2) });
+  GltfContainer.create(uiDisplay, { src: "models/screen.gltf", visibleMeshesCollisionMask: ColliderLayer.CL_POINTER } );
+  await waitUntilGltfIsLoaded([uiDisplay])
+  
+  console.log("paths:")
+  GltfContainerLoadingState.get(uiDisplay).nodePaths.forEach((p) => {
+    console.log(p)
+  });
+  console.log("end paths")
 
-    console.log("make screen");
-    const screen = engine.addEntity();
-    Transform.createOrReplace(screen, { parent: uiDisplay });
-    GltfNode.create(screen, { path: "Plane/Plane" });
-    await waitUntilGltfNodeIsLoaded([screen]);
+  console.log("make screen");
+  const screen = engine.addEntity();
+  Transform.createOrReplace(screen, { parent: uiDisplay });
+  GltfNode.create(screen, { path: "Plane/Plane" });
+  await waitUntilGltfNodeIsLoaded([screen]);
 
-    console.log(`has mat?: ${Material.has(screen)}`);
+  console.log(`has mat?: ${Material.has(screen)}`);
 
-    if (use_texture) {
-      Material.setPbrMaterial(screen, {
-        texture: {
-          tex: {
-            $case: 'uiTexture',
-            uiTexture: {
-              uiCanvasEntity: uiCanvas,
-            }
+  if (useTexture) {
+    Material.setPbrMaterial(screen, {
+      texture: {
+        tex: {
+          $case: 'uiTexture',
+          uiTexture: {
+            uiCanvasEntity: uiCanvas,
           }
         }
-      })  
-    }
+      }
+    })  
   }
+}
   
 async function init(): Promise<void> {
   await Promise.all([setupScroll()])
--- lights/src/index.ts
+++ lights/src/index.ts
@@ -1,75 +1,74 @@
+import { ColliderLayer, engine, GlobalLight, GltfContainer, Light, Material, MeshRenderer, Schemas, Transform } from "@dcl/sdk/ecs";
+import { Color4, Vector3 } from "@dcl/sdk/math";
 
-import { Billboard, ColliderLayer, engine, GltfContainer, GltfContainerLoadingState, GltfNode, Material, TextShape, Transform, UiCanvas } from "@dcl/sdk/ecs"
-import { Color4, Vector3 } from "@dcl/sdk/math"
-import { ReactEcsRenderer } from "@dcl/sdk/react-ecs"
-import { UiExample } from "./ui"
-import { waitUntilGltfIsLoaded, waitUntilGltfNodeIsLoaded } from "./utils"
+const LightMarker = engine.defineComponent("LightMarker", { offset: Schemas.Number });
 
 export function main(): void {
-  init().catch(console.error)
+	init().catch(console.error)
 }
 
-async function setupScroll() {
-    const use_texture = true;
+function createLight(color: Color4, offset: number) {
+	const light = engine.addEntity();
 
-    const ui = new UiExample()
+	// setting a light on a non-root entity creates a point light
+	Light.createOrReplace(light, { illuminance: 100000, shadows: true, color: color })
+	
+	// we can add spotlight angles to make it a spotlight instead
+	// Spotlight.createOrReplace(light, {angle: 3.141592 / 4})
 
-    const uiCanvas = engine.addEntity();
-    if (use_texture) {
-      UiCanvas.create(uiCanvas, { width: 1024, height: 1024, color: Color4.Black() });
-      ReactEcsRenderer.setTextureRenderer(uiCanvas, ui.render.bind(ui));
-    } else {
-      ReactEcsRenderer.setUiRenderer(ui.render.bind(ui));
-    }
+	// visual representation of the light
+	MeshRenderer.setSphere(light);
+	Material.setPbrMaterial(light, {albedoColor: color, emissiveColor: color, castShadows: false})
+	
+	// just for the movement system
+	LightMarker.createOrReplace(light, { offset })	
 
-    const textShapeEntity = engine.addEntity();
-    TextShape.create(textShapeEntity, {
-        text: "Scrollable UI - Example",
-        fontSize: 20,
-        textColor: Color4.Red(),
-        outlineColor: Color4.Black(),
-        outlineWidth: 0.2
-    });
-    Transform.create(textShapeEntity, {
-      position: Vector3.create(8, 1, 8)
-    });
-    Billboard.create(textShapeEntity);
+	// placing it in the scene
+	Transform.createOrReplace(light, { position: Vector3.create(8, 6.5, 0), scale: Vector3.create(0.2,0.2,0.2) })
+}
 
-    console.log("make display");
-    const uiDisplay = engine.addEntity();
-    Transform.create(uiDisplay, { position: Vector3.create(8,1,8), scale: Vector3.create(2,2,2) });
-    GltfContainer.create(uiDisplay, { src: "models/screen.gltf", visibleMeshesCollisionMask: ColliderLayer.CL_POINTER } );
-    await waitUntilGltfIsLoaded([uiDisplay])
-    
-    console.log("paths:")
-    GltfContainerLoadingState.get(uiDisplay).nodePaths.forEach((p) => {
-      console.log(p)
-    });
-    console.log("end paths")
+function updateLights(dt: number) {
+	for (const [entity] of engine.getEntitiesWith(LightMarker)) {
+		const transform = Transform.getMutable(entity) 
+		const marker = LightMarker.getMutable(entity)
+		marker.offset += dt / 2
+		transform.position.z = Math.sin(marker.offset) * 2.5 + 3
+		transform.position.x = Math.cos(marker.offset) * 2.5 + 8
+	}
+}
 
-    console.log("make screen");
-    const screen = engine.addEntity();
-    Transform.createOrReplace(screen, { parent: uiDisplay });
-    GltfNode.create(screen, { path: "Plane/Plane" });
-    await waitUntilGltfNodeIsLoaded([screen]);
-
-    console.log(`has mat?: ${Material.has(screen)}`);
-
-    if (use_texture) {
-      Material.setPbrMaterial(screen, {
-        texture: {
-          tex: {
-            $case: 'uiTexture',
-            uiTexture: {
-              uiCanvasEntity: uiCanvas,
-            }
-          }
-        }
-      })  
-    }
-  }
-  
 async function init(): Promise<void> {
-  await Promise.all([setupScroll()])
+	// setting a light on the root entity controls the sunlight brightness / color / shadows
+	Light.createOrReplace(engine.RootEntity, {
+		illuminance: 1000,  // brightness
+		shadows: true,  // cast shadows
+		color: Color4.Red()
+	})
+	
+	// on the root we can also control the sunlight direction and the ambient light color/brightness
+	GlobalLight.createOrReplace(engine.RootEntity, {
+		direction: Vector3.Down(),
+		ambientColor: Color4.create(0.0, 0.0, 0.0), // ambient color
+		ambientBrightness: 1,
+	})
+
+	createLight(Color4.Red(), 0);
+	createLight(Color4.Green(), 2 * Math.PI / 3);
+	createLight(Color4.Blue(), 4 * Math.PI / 3);
+	console.log("made lights")
+
+	engine.addSystem(updateLights) 
+
+	const planePlatform = engine.addEntity();
+	MeshRenderer.setBox(planePlatform);
+	Transform.createOrReplace(planePlatform, { position: Vector3.create(9, 2.5, 3), scale: Vector3.create(0.5, 3.0, 0.5)});
+
+	const monkehEntity = engine.addEntity() 
+	GltfContainer.create(monkehEntity, { 
+		src: 'models/monkeh.gltf',
+		visibleMeshesCollisionMask: ColliderLayer.CL_PHYSICS
+	})
+	Transform.create(monkehEntity, { 
+		position: Vector3.create(8, 0, 8), 
+	})
 }
-  
